6e461c8b3181e61c2055b32855e55bc9a0035366
itasouza <itasouza@yahoo.com.br>
2017-06-15 01:07:35 -0300

    Editor de Texto no MVC

---
+++ Projeto.Web.RichTextEditor/Projeto.Web.RichTextEditor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto.Web.RichTextEditor.MvcApplication" Language="C#" %>
---
+++ Projeto.Web.RichTextEditor/Projeto.Web.RichTextEditor/Models/RichTextEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projeto.Web.RichTextEditor.Models
+{
+    public class RichTextEditorViewModel
+    {
+        [AllowHtml]
+        [Display(Name = "Mensagem:")]
+        public string Message {get;set;}
+
+    }
+}
